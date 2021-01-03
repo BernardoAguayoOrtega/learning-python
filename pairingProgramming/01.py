3fc76447f97972412b0505668c8352f938785710
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-02 20:10:08 -0600

    [create] create new file

---
+++ pairingProgramming/01.py
@@ -0,0 +1,33 @@
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3
+
+# jewels = "aA"
+
+# stones = "aAAbbbb"
+
+# counter = 0
+
+# for x in stones:
+#   for y in jewels:
+#     if(x == y):
+#       counter = counter + 1
+
+# print(counter)
+
+# # function that filters vowels 
+# def fun(variable): 
+#     letters = ['a', 'A', 'A', 'b', 'b'] 
+#     if (variable in letters): 
+#         return True
+#     else: 
+#         return False
+
+# print(fun(['A','a']))
+
+# 'aA' in stones
+
+# print('aA' in stones)
