b6bf2c3d2153747231bc51d926d4dd62b2008cf0
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-04 19:44:43 -0600

    [update] finish classes file

--- ch2/classes.py
+++ ch2/classes.py
@@ -2,8 +2,30 @@
 # Example file for working with classes
 #
 
+class myClass():
+  def method1(self):
+    print('myCLass method1')
+    
+  def method2(self, someString):
+    print('myCLass method2' + someString)
+
+class anotherClass(myClass):
+  
+  def method1(self):
+    myClass.method1(self)
+    print('anotherClass method1')
+    myClass.method2(self, 'someString')
+    
+  def method2(self):
+    print('anotherClass method2')
 
 def main():
+  c = myClass()
+  
+  c2 = anotherClass()
+  
+  c2.method1()
+  c2.method2()  
 
 
 if __name__ == "__main__":
