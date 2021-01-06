7f9caf90618fdffa04e0ca4a6903724b64f8a0fe
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-06 16:57:20 -0600

    [update]

--- ch3/calendars.py
+++ ch3/calendars.py
@@ -18,14 +18,25 @@ print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
-  
+for i in c.itermonthdays(2020, 1):
+  print (i)
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
-
+for name in calendar.month_name:
+  print(name)
+  
+for day in calendar.day_name:
+  print(day)
+  
+  print(' ')
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
+print('Team meetings will be on: ')
+for m in range(1, 13):
+  cal = calendar.monthcalendar(2020, m)
 
+  weekone=cal[0]
+  weektwo=cal[1]
