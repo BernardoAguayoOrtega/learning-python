1f7fc0cc3fc7c3726b9361a817f242d0e7a97d6e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-07 09:46:04 -0600

    [update] add examples for ospathutils file

--- ch4/ospathutils.py
+++ ch4/ospathutils.py
@@ -10,14 +10,21 @@ import time
 
 def main():
   # Print the name of the OS
-
+  print(os.name)
   
   # Check for item existence and type
-
+  print('Item exists: ' + str(path.exists('textfile.txt')))
+  print('Item is a file: ' + str(path.isfile('textfile.txt')))
+  print('Item is a dir: ' + str(path.isdir('textfile.txt')))
   
   # Work with file paths
+  print('item path: ' + str(
+    path.realpath('textfile.txt')
+  ))
 
-  
+  print('item path and name ' + str(
+    path.split(path.realpath('textfile.txt'))
+  ))
   # Get the modification time
 
   
