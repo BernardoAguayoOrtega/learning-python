0a2ad29db92cd39c294c266bd115b92ba6084066
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-08 17:49:37 -0600

    [create] create html parcing file

---
+++ ch5/htmlparcing.py
@@ -0,0 +1,17 @@
+# 
+# Example file for parsing and processing HTML
+#
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+  
+
+def main():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+    
+
+if __name__ == "__main__":
+  main();
+  
