5d12d74501ec65691ee874b3f146d453df950602
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-08 09:31:34 -0600

    [create] create inetdata file

---
+++ ch5/inetdata.py
@@ -0,0 +1,17 @@
+# 
+# Example file for retrieving data from the internet
+#
+
+import urllib.request
+
+def main():
+  webUrl = urllib.request.urlopen("http://google.com")
+  
+  print("result code: " + str(webUrl.getcode()))
+  
+  data = webUrl.read()
+  
+  print(data)
+
+if __name__ == "__main__":
+  main()
